f7fdfc67cb481b435efe38697a2efadd1c615385
llau08 <llau251@gmail.com>
2021-10-22 02:27:20 -0700

    updated bio page w/ forEach

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,9 +3,15 @@ const apiKey = "1158e45f-b3ed-4a56-852a-8d3d3cc13661";
 
 axios.get(`${apiURL}comments/?api_key=${apiKey}`).then(response =>{
 
-    // response.data.forEach((element,i) => {
-    //     console.log(element);
- 
+    response.data.forEach((element, index) => {
+        if (index <= 2) {
+            console.log(element);
+            displayOriginalComment(element);
+        } else {
+            console.log(element);
+            displayNewComment(element);
+        }
+            
     // for (let i=response.data.length -1; i >= 3; i--){
     //     console.log(i);
     //     displayComment(response.data[i]);
@@ -13,8 +19,10 @@ axios.get(`${apiURL}comments/?api_key=${apiKey}`).then(response =>{
     // for (let i=0; i <= 2; i++){
     //     displayComment(response.data[i]);
     // }
-});
-
+    }) 
+}).catch(function (error) {
+    console.log(error);
+  });
 const commentBox = document.querySelector('.convo__comments');
 const commentTextBox = document.getElementById('comment');
 const commentNameBox = document.getElementById('name');
@@ -35,10 +43,59 @@ const commentNameBox = document.getElementById('name');
 //     note: "I can't stop listening. Every time I hear one of their songs-the vocals-it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
 // }];
 
-function displayComment(comm){
-const singleComm = document.createElement('article');
-singleComm.classList.add('convo__indv-box');
-commentBox.appendChild(singleComm);
+// function displayComment(comm){
+// const singleComm = document.createElement('article');
+// singleComm.classList.add('convo__indv-box');
+// commentBox.appendChild(singleComm);
+// commentBox.prepend(singleComm);
+
+// // //IMAGE BOX//
+// const commImg = document.createElement('div');
+// commImg.classList.add('convo__img-box');
+// singleComm.appendChild(commImg);
+
+// //ADD IMAGE TO IMG BOX//
+// const avatar = document.createElement('div');
+// avatar.classList.add('convo__avatar');
+// commImg.appendChild(avatar);
+
+// //THAT BOX THAT HOLDS THE NAME/DATE/TEXT TOGETHER//
+// const innerCommBox = document.createElement('div');
+// innerCommBox.classList.add('convo__sub-box')
+// singleComm.appendChild(innerCommBox);
+
+// //NAME & DATE BOX//
+// const commHead = document.createElement('div');
+// commHead.classList.add('convo__indv-head');
+// innerCommBox.appendChild(commHead);
+
+// ////INNER COMMENT TXT//
+// const commTxt = document.createElement('div');
+// commTxt.classList.add('convo__txt');
+// innerCommBox.appendChild(commTxt);
+
+// //INNER COMMENT P TAGS//
+// const pName = document.createElement('p');
+// pName.classList.add('convo__comm-name')
+// commHead.appendChild(pName);
+// pName.innerText = comm.name;
+
+// const pDate = document.createElement('p');
+// pDate.classList.add('convo__comm-date')
+// commHead.appendChild(pDate);
+// pDate.innerText = changeDate(comm.timestamp);
+
+// const pTxt = document.createElement('p');
+// pTxt.classList.add('convo__comm-txt')
+// commTxt.appendChild(pTxt);
+// pTxt.innerText = comm.comment;
+// }
+
+function displayComment(comm, singleComm) {
+// const singleComm = document.createElement('article');
+// singleComm.classList.add('convo__indv-box');
+// commentBox.appendChild(singleComm);
+// commentBox.prepend(singleComm);
 
 // //IMAGE BOX//
 const commImg = document.createElement('div');
@@ -82,6 +139,22 @@ commTxt.appendChild(pTxt);
 pTxt.innerText = comm.comment;
 }
 
+function displayOriginalComment(comm) {
+    const singleComm = document.createElement('article');
+    singleComm.classList.add('convo__indv-box');
+    commentBox.appendChild(singleComm);
+    commentBox.appendChild(singleComm);
+    displayComment(comm, singleComm);
+}
+
+function displayNewComment(comm) {
+    const singleComm = document.createElement('article');
+    singleComm.classList.add('convo__indv-box');
+    commentBox.appendChild(singleComm);
+    commentBox.prepend(singleComm);
+    displayComment(comm, singleComm);
+}
+
 
 
 // for (let i=0; i < comments.length; i++){
@@ -121,14 +194,23 @@ form.addEventListener('submit', function (event) {
       })
       .then (function (response){
         axios.get(`${apiURL}comments/?api_key=${apiKey}`).then(response =>{
-            console.log(response.data.length -1);
-            for (let i=response.data.length -1; i >= 3; i--){
-                console.log(i);
-                displayComment(response.data[i]);
-            }
-            for (let i=0; i <= 2; i++){
-                displayComment(response.data[i]);
-            }
+            response.data.forEach((element, index) => {
+                if (index <= 2) {
+                    console.log(element);
+                    displayOriginalComment(element);
+                } else {
+                    console.log(element);
+                    displayNewComment(element);
+                }
+       
+            // console.log(response.data.length -1);
+            // for (let i=response.data.length -1; i >= 3; i--){
+            //     console.log(i);
+            //     displayComment(response.data[i]);
+            // }
+            // for (let i=0; i <= 2; i++){
+            //     displayComment(response.data[i]);
+            // }
       })
     })
       .catch(function (error) {
@@ -144,6 +226,5 @@ form.addEventListener('submit', function (event) {
 commentTextBox.addEventListener("click", () => {
     commentTextBox.innerText = "";
 })
-axios.put(`${apiURL}`),{
-    
-}
+
+})
