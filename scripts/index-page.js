7c4160da5389ddcecaaf49f74f88f93f21b3265b
llau08 <llau251@gmail.com>
2021-10-22 16:24:15 -0700

    removed extra comments

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -11,14 +11,6 @@ axios.get(`${apiURL}comments/?api_key=${apiKey}`).then(response =>{
             console.log(element);
             displayNewComment(element);
         }
-            
-    // for (let i=response.data.length -1; i >= 3; i--){
-    //     console.log(i);
-    //     displayComment(response.data[i]);
-    // }
-    // for (let i=0; i <= 2; i++){
-    //     displayComment(response.data[i]);
-    // }
     }) 
 }).catch(function (error) {
     console.log(error);
@@ -27,75 +19,8 @@ const commentBox = document.querySelector('.convo__comments');
 const commentTextBox = document.getElementById('comment');
 const commentNameBox = document.getElementById('name');
 
-// let comments = [{
-//     name: 'Connor Walton',
-//     date: '02/17/2021',
-//     note: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-// },
-// {
-//     name: 'Emilie Beach',
-//     date: '01/09/2021',
-//     note: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-// },
-// {
-//     name: 'Miles Acosta',
-//     date: '12/20/2020',
-//     note: "I can't stop listening. Every time I hear one of their songs-the vocals-it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-// }];
-
-// function displayComment(comm){
-// const singleComm = document.createElement('article');
-// singleComm.classList.add('convo__indv-box');
-// commentBox.appendChild(singleComm);
-// commentBox.prepend(singleComm);
-
-// // //IMAGE BOX//
-// const commImg = document.createElement('div');
-// commImg.classList.add('convo__img-box');
-// singleComm.appendChild(commImg);
-
-// //ADD IMAGE TO IMG BOX//
-// const avatar = document.createElement('div');
-// avatar.classList.add('convo__avatar');
-// commImg.appendChild(avatar);
-
-// //THAT BOX THAT HOLDS THE NAME/DATE/TEXT TOGETHER//
-// const innerCommBox = document.createElement('div');
-// innerCommBox.classList.add('convo__sub-box')
-// singleComm.appendChild(innerCommBox);
-
-// //NAME & DATE BOX//
-// const commHead = document.createElement('div');
-// commHead.classList.add('convo__indv-head');
-// innerCommBox.appendChild(commHead);
-
-// ////INNER COMMENT TXT//
-// const commTxt = document.createElement('div');
-// commTxt.classList.add('convo__txt');
-// innerCommBox.appendChild(commTxt);
-
-// //INNER COMMENT P TAGS//
-// const pName = document.createElement('p');
-// pName.classList.add('convo__comm-name')
-// commHead.appendChild(pName);
-// pName.innerText = comm.name;
-
-// const pDate = document.createElement('p');
-// pDate.classList.add('convo__comm-date')
-// commHead.appendChild(pDate);
-// pDate.innerText = changeDate(comm.timestamp);
-
-// const pTxt = document.createElement('p');
-// pTxt.classList.add('convo__comm-txt')
-// commTxt.appendChild(pTxt);
-// pTxt.innerText = comm.comment;
-// }
 
 function displayComment(comm, singleComm) {
-// const singleComm = document.createElement('article');
-// singleComm.classList.add('convo__indv-box');
-// commentBox.appendChild(singleComm);
-// commentBox.prepend(singleComm);
 
 // //IMAGE BOX//
 const commImg = document.createElement('div');
@@ -156,11 +81,6 @@ function displayNewComment(comm) {
 }
 
 
-
-// for (let i=0; i < comments.length; i++){
-//     displayComment(comments[i]);
-// };
-
 function changeDate(timestamp){
     let today = new Date(Number.parseInt(timestamp));
 
@@ -202,15 +122,7 @@ form.addEventListener('submit', function (event) {
                     console.log(element);
                     displayNewComment(element);
                 }
-       
-            // console.log(response.data.length -1);
-            // for (let i=response.data.length -1; i >= 3; i--){
-            //     console.log(i);
-            //     displayComment(response.data[i]);
-            // }
-            // for (let i=0; i <= 2; i++){
-            //     displayComment(response.data[i]);
-            // }
+
       })
     })
       .catch(function (error) {
@@ -226,5 +138,4 @@ form.addEventListener('submit', function (event) {
 commentTextBox.addEventListener("click", () => {
     commentTextBox.innerText = "";
 })
-
 })
